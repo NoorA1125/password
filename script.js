2eb82092f35332213938b6b57da5db98197480c6
Noor Abdikadir <nooromar305@gmail.com>
2021-03-10 23:08:25 -0700

    Updated the new password length being manually changed through user input. Thanks to the help of a tutor, Queen. All is well and functional now. Only one more thing remaining: Check boxes

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-// Assignment Code
 var generateBtn = document.getElementById("generate");
-var passwordLength = document.getElementById("passLength");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var capCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbersList = "0123456789";
-var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var userInput = "";
-//charset = lowerCase.concat(capCase,numbersList,specialCharacters);
-//var userInput = Prompt.render('Enter A Password To Encrypt:','changeText');
-charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var capCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"], numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", " \ ", "]", "^", "_", "`", "{", "|", "}", "~"];
+var charset = capCase.concat(lowerCase, numbersList, specialCharacters);
+var passLength;
+//let test = parseInt(passLength)
 
+
+//var passLength = document.getElementById("passLength").value;
 //Before generating password, run this function to see if requirements are met.
 function customPrompt() {
-  userInput = Prompt.render('Enter A Password To Encrypt:', 'generatePassword'); //popup called userInput
-  if (userInput >= passwordLength.length) { //If the vlaue of userInout >= 8, then 
+  //var passwordLength  = document.querySelector("#passLength");
+  //passwordLength.innerHTML.value = newPasslength;
+
+  var userInput = Prompt.render('Enter A Password To Encrypt:', 'generatePassword'); //popup called userInput
+  if (userInput >= passLength) { //If the vlaue of userInput >= 8, then 
     generatePassword();
   }
 }
 
 // Run this function to create the encrypted password
-function generatePassword() {
+/*function generatePassword() {
   var passwordString = "";
-  for (var i = 0; i < passwordLength; i++) {//don't let i exceed 8 characters which is password length
+  console.log("reg", passLength)
+  for (var i = 0; i < passLength; i++) {//don't let i exceed 8 characters which is password length
     var randomNumber = Math.floor(Math.random() * charset.length);
     passwordString += charset[randomNumber] //targets the charsets array to get the characters in a random order
   }
   return passwordString;
 }
+*/
+//If Options are selected
+function checkBoxes() {
+  var caps = document.getElementById("check1").checked;
+  var lower = document.getElementById("check2").checked;
+  var num = document.getElementById("check3").checked;
+  var charcters = document.getElementById("check4").checked;
 
-function promptContainer() {
+  if (caps = true) {
+    charset = lowerCase.concat(numbersList, specialCharacters).value;
+    console.log(charset);
+    console.log("passlength3", passLength)
+  }
+
+}
+
+function promptContainer(passLength, userInput) {
   this.render = function (dialog, func) {
     /*Sizing width container */
     var winW = window.innerWidth;
@@ -46,27 +61,61 @@ function promptContainer() {
     document.getElementById('dialogboxhead').innerHTML = "A value is required";
     document.getElementById('dialogboxbody').innerHTML = dialog;
     document.getElementById('dialogboxbody').innerHTML += '<br><input id="userInput">';
-    document.getElementById('dialogboxfoot').innerHTML = '<div class="checkbox-container"> Password Length: <input id="passLength" maxlength="2" size="2"><input type="checkbox" value="Caps"> Caps <input type="checkbox" value="lowercase"> Lower <input type="checkbox" value="Num"> Numbers <input type="checkbox" value="Special"> Characters </div> <br><button class="button button1" onclick="Prompt.ok(\'' + func + '\')">OK</button> <button class="button button2" onclick="Prompt.cancel()">Cancel</button>';
+    document.getElementById('dialogboxfoot').innerHTML = '<div class="checkbox-container"> Password Length: <input id="passLength" maxlength="2" size="2"><input  id="check1" type="checkbox" value="Caps"> Caps <input id="check2" type="checkbox" value="lowercase"> Lower <input id="check3" type="checkbox" value="Num"> Numbers <input id="check4" type="checkbox" value="Special"> Characters </div> <br><button class="button button1" onclick="Prompt.ok(\'' + func + '\')">OK</button> <button class="button button2" onclick="Prompt.cancel()">Cancel</button>';
+
+
+
+
+
+
+    passLength = document.getElementById("passLength").value;
+    console.log("passlength", passLength)
+
   }
+
   /* End of Styling prompt box */
   this.cancel = function () {
     document.getElementById('dialogbox').style.display = "none";
     document.getElementById('dialogoverlay').style.display = "none";
   }
-  this.ok = function (func) {
+  this.ok = function (func, passLength) {
+    passLength = parseInt(document.getElementById("passLength").value);
+    console.log("passlength2", passLength)
     var userInput = document.getElementById("userInput").value;
-    if (userInput.length >= passwordLength.length) {
+    //var newPasslength = document.getElementById("passLength").value;
+    // Run this function to create the encrypted password
+
+    let newPass = passLength
+    function generatePassword(passLength) {
+      var passwordString = "";
+      console.log("reg", newPass)
+      for (var i = 0; i < newPass; i++) {//don't let i exceed 8 characters which is password length
+        var randomNumber = Math.floor(Math.random() * charset.length);
+        passwordString += charset[randomNumber] //targets the charsets array to get the characters in a random order
+      }
+      return passwordString;
+    }
+
+    if (userInput.length >= newPass) {
       var password = generatePassword();
-      var passwordText = document.getElementById("password");
+      var passwordText = document.querySelector("#password");
       passwordText.value = password;
       document.getElementById('dialogbox').style.display = "none";
       document.getElementById('dialogoverlay').style.display = "none";
+      
+
+      console.log("passlength4", passLength)
+      exefunction();
     } else {
-      alert("Please make sure your lenth is 8 characters long");
+      alert("Please make sure your lenth is " + passLength + " characters long");
+      console.log(passLength);
     }
   }
 }
+
+
 var Prompt = new promptContainer();
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", promptContainer);
